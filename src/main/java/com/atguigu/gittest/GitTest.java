927b677f4515218c008bed277d53f22e854a21b9
jason <jason@atguigu.com>
2021-08-27 20:46:26 +0800

    master commit

--- src/main/java/com/atguigu/gittest/GitTest.java
+++ src/main/java/com/atguigu/gittest/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello data");
         System.out.println("hello big");
+        System.out.println("master test");
     }
 }
